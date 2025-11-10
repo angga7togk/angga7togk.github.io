42024117e387c63d2a99ce0cb54b385c09212b19
Angga7Togk <kiplihode123321@gmail.com>
2025-11-10 20:18:14 +0700

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    name: "GamanJS, Backend Application Framework",
+    icon: "gamanjs.png",
+  },
+  {
+    name: "Kota Roleplay, Website Roblox Game Roleplay",
+    icon: "kota-roleplay.png",
+  },
+  {
+    name: "Exam Mutu, online exam application",
+    icon: "mutu.png",
+  },
+  {
+    name: "PT. Shadikara, Website PT. Shadikara",
+    icon: "ptshadikara.png",
+  },
+  {
+    name: "Skyhosting, Website Growtopia Game Server",
+    icon: "skyhosting.png"
+  },
+  {
+    name: "7TogkID, Organization",
+    icon: "7togkid.png",
+  },
+  {
+    name: "7TogkNetwork, Server Minecraft Indonesia",
+    icon: "7togknetwork.jpg",
+  },
+  {
+    name: "7TogkSMP, Server Minecraft, Survival Multiplayer",
+    icon: "7togksmp.png",
+  },
+  {
+    name: "Animaku, Streaming And Download Anime Sub Indo!",
+    icon: "animaku.png"
+  }
+];
